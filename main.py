77eb3a3de125c8d665ad3fe88bcc312193d82485
engageintellect <jc9361@gmail.com>
2023-09-15 18:55:34 -0700

    init

--- main.py
+++ main.py
@@ -8,21 +8,12 @@ import uvicorn
 app = FastAPI()
 
 # Pydantic model for request body
-class TextForClassification(BaseModel):
-    text: str
-    candidate_labels: "List[str]"
 
 
 # Initialize the classifier once, outside of the endpoint, for better performance
-classifier = pipeline('zero-shot-classification')
 
-@app.post("/")
-def classify_text(request_data: TextForClassification):
-    try:
-        res = classifier(request_data.text, candidate_labels=request_data.candidate_labels)
-        return res
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@app.get("/")
+        return {"hello world"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
